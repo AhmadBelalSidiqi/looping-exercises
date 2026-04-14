e9b9c5eb369aa0909c8df797087a9727a4626a89
ahmadbelalsidiqi <Ahmad.sidiqiNe@gmail.com>
2026-04-14 12:43:50 -0400

    Step 1 and 2 Added.

--- src/main/java/com/pluralsight/WhileLoop.java
+++ src/main/java/com/pluralsight/WhileLoop.java
@@ -1,4 +1,9 @@
 package com.pluralsight;
 
 public class WhileLoop {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            System.out.println("I love Java");
+        }
+    }
 }
